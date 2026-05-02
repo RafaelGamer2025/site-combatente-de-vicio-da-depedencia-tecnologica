8330bf65018b9ba33ff4273ccaf6b235556a3774
RafaelGamer2025 <rafaelferrao2016@gmail.com>
2026-05-01 23:42:52 -0300

    fazendo um site2

--- aula1/app.py
+++ aula1/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from pyngrok import ngrok
 
 app = Flask(__name__)
 
@@ -13,7 +12,4 @@ def combater():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=10000)
